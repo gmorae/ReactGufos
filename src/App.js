7740946d355605a235766eeaa284554185a39130
gmorae <gmmartins06@gmail.com>
2019-11-12 15:53:36 -0200

    Página Inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/flexbox.css';
+import './assets/css/reset.css';
+import './assets/css/style.css';
+import './assets/css/cabecalho.css';
+import logo from './assets/img/icon-login.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="cabecalhoPrincipal">
+        <div className="container">
+          <img src={logo} alt="logo" />
+
+          <nav className="cabecalhoPrincipal-nav">
+            <a>Home</a>
+            <a>Eventos</a>
+            <a>Contato</a>
+            <a className="cabecalhoPrincipal-nav-login" href="login.html">Login</a>
+          </nav>
+        </div>
       </header>
+
+      <section className="conteudoImagem">
+        <div>
+          <h1>Gufos</h1>
+          <h2>Área de eventos da Escola SENAI de Informática.</h2>
+        </div>
+      </section>
+
+      <main className="conteudoPrincipal">
+        <section id="conteudoPrincipal-eventos">
+          <h1 id="conteudoPrincipal-eventos-titulo">Próximos Eventos</h1>
+          <div className="container">
+            <nav>
+              <ul className="conteudoPrincipal-dados">
+                <li className="conteudoPrincipal-dados-link eventos">
+                  <h2>Título do Evento</h2>
+                  <p>
+                    Breve descrição sobre o evento. Lorem ipsum lorem ipsum
+                    lorem ipsum.
+                  </p>
+                  <button>conectar</button>
+                </li>
+
+                <li className="conteudoPrincipal-dados-link eventos">
+                  <h2>Título do Evento</h2>
+                  <p>
+                    Breve descrição sobre o evento. Lorem ipsum lorem ipsum
+                    lorem ipsum.
+                  </p>
+                  <button>conectar</button>
+                </li>
+
+                <li className="conteudoPrincipal-dados-link eventos">
+                  <h2>Título do Evento</h2>
+                  <p>
+                    Breve descrição sobre o evento. Lorem ipsum lorem ipsum
+                    lorem ipsum.
+                  </p>
+                  <button>conectar</button>
+                </li>
+
+                <li className="conteudoPrincipal-dados-link eventos">
+                  <h2>Título do Evento</h2>
+                  <p>
+                    Breve descrição sobre o evento. Lorem ipsum lorem ipsum
+                    lorem ipsum.
+                  </p>
+                  <button>conectar</button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </section>
+
+        <section id="conteudoPrincipal-visao">
+          <h1 id="conteudoPrincipal-visao-titulo">Por Quê Participar?</h1>
+          <div className="container">
+            <p className="visao-texto">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+              Nullam auctor suscipit eros sed blandit. <br />
+              Fusce euismod neque sed dapibus sollicitudin. <br />Duis vel lacus
+              vestibulum, molestie dui eu, bibendum nunc.
+            </p>
+          </div>
+        </section>
+
+        <section id="conteudoPrincipal-contato">
+          <h1 id="conteudoPrincipal-contato-titulo">Contato</h1>
+          <div className="container conteudo-contato-titulo">
+            <div className="contato-mapa conteudo-contato-mapa"></div>
+            <div className="contato-endereco conteudo-contato-endereco">
+              Alameda Barão de Limeira, 539 <br />
+              São Paulo - SP
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
